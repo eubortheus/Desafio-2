b1a4e8c0a3a7e01e986499c86a3624debd2ea746
eubortheus <imatthborba@gmail.com>
2022-02-07 22:13:39 -0300

    Finalização do Desafio

---
+++ js/promocao.js
@@ -0,0 +1,14 @@
+const produtoPromocao1 = document.querySelector('[data-card-text-1]');
+
+produtoPromocao1.addEventListener('mouseover', ()=> {
+produtoPromocao1.innerHTML = "Por 10$ o prato" });
+
+const produtoPromocao2 = document.querySelector('[data-card-text-2]');
+
+produtoPromocao2.addEventListener('mouseover', ()=> {
+produtoPromocao2.innerHTML = "Por 20$ a garrafa ou 4.5$ o copo" });
+
+const produtoPromocao3 = document.querySelector('[data-card-text-3]');
+
+produtoPromocao3.addEventListener('mouseover', ()=> {
+produtoPromocao3.innerHTML =  "Por 9$ a fatia"});
